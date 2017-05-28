8ef978a65ef57f2e6a254dfad97d615d51f23934
kysam2 <kylehsoccer@gmail.com>
2017-05-28 14:57:07 -0400

    Add files via upload

---
+++ BlueSky_pygame.py
@@ -0,0 +1,41 @@
+import pygame as pg
+from bluesky import sim, scr, stack
+from bluesky.ui.pygame import Keyboard, splash
+from bluesky.tools import plugin
+if __name__ == "__main__":
+    print "   *****   BlueSky Open ATM simulator *****"
+    print "Distributed under GNU General Public License v3"
+
+
+def MainLoop():
+    # =============================================================================
+    # Start the mainloop (and possible other threads)
+    # =============================================================================
+    splash.show()
+    plugin.init()
+    stack.init()
+    sim.start()
+    scr.init()
+
+    # Main loop for tmx object
+    while not sim.mode == sim.end:
+        sim.update()   # Update sim
+        scr.update()   # GUI update
+
+        # Restart traffic simulation:
+        if sim.mode == sim.init:
+            sim.reset()
+            scr.objdel()     # Delete user defined objects
+
+    # After the simulation is done, close the gui
+    sim.stop()
+    pg.quit()
+    print 'BlueSky normal end.'
+    return
+
+#==============================================================================
+
+# Run mainloop if BlueSky_pygame is called directly
+
+if __name__ == '__main__':
+    MainLoop()
